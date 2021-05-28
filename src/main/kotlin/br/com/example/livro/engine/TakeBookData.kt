1025fb05f134ae2d814fd6ac4b72d888d70760db
whoismarviin <vini.rosa.roo@gmail.com>
2021-05-28 15:16:46 -0400

    ajustes

--- src/main/kotlin/br/com/example/livro/engine/TakeBookData.kt
+++ src/main/kotlin/br/com/example/livro/engine/TakeBookData.kt
@@ -1,13 +1,15 @@
-//package br.com.example.livro.engine
-//
-//import io.micronaut.http.annotation.Get
-//import java.net.http.HttpResponse
-//
-//
-//class TakeBookData {
-//
-//    @Get("api/livros")
-//    fun buscaLivros():HttpResponse<Any>{
-//        val livros =
-//    }
-//}
+package br.com.example.livro.engine
+
+import br.com.example.livro.engine.database.EngineRepository
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Get
+
+
+class TakeBookData(private val repository: EngineRepository) {
+
+    @Get("/")
+    fun buscaLivros(): HttpResponse<Any> {
+        val livros = repository.buscaLivros()
+        return HttpResponse.ok(livros)
+    }
+}
